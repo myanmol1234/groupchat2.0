2c93ae5176a1a21a68948585bb0402f4dd7f9977
myanmol1234 <anmolgoyal981997@gmail.com>
2022-10-06 11:25:16 +0530

    group dashboard added

--- routes/user1.js
+++ routes/user1.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router=express.Router()
 const UserController=require('../controllers/user')
 router.post('/signup', UserController.signup);
-router.post('login',UserController.login);
+router.post('/login',UserController.login);
 
 module.exports=router;
